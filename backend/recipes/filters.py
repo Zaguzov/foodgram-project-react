472376b09348fae6e4fca49978231ea07255e159
Zaguzov <zaguzovalex@gmail.com>
2021-10-01 17:24:30 +0300

    Done

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -7,7 +7,7 @@ class RecipeFilter(filters.FilterSet):
     tags = filters.AllValuesMultipleFilter(
         field_name='receipttag__tag__slug'
     )
-    is_favorited = filters.BooleanFilter(
+    is_favorite = filters.BooleanFilter(
         method='get_favorite'
     )
     is_in_shopping_cart = filters.BooleanFilter(
@@ -17,7 +17,7 @@ class RecipeFilter(filters.FilterSet):
     class Meta:
         model = Recipe
         fields = (
-            'is_favorited',
+            'is_favorite',
             'is_in_shopping_cart',
             'author',
             'tags'
