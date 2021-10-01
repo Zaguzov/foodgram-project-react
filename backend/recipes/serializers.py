8362d5ad64ae1cc1a88aa0d20131057ff40f6d39
Zaguzov <zaguzovalex@gmail.com>
2021-10-01 10:41:35 +0300

    Done

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -62,7 +62,7 @@ class IngredientSerializer(serializers.ModelSerializer):
 class IngredientInRecipeSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField(source='ingredient.id')
     name = serializers.ReadOnlyField(source='ingredient.name')
-    unit = serializers.ReadOnlyField(
+    measurement_unit = serializers.ReadOnlyField(
         source='ingredient.measurement_unit'
     )
 
