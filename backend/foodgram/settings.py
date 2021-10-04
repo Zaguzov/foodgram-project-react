e8c1a3b1c3b4e499c9bc65cb285ab658cc2cfb13
Zaguzov <zaguzovalex@gmail.com>
2021-10-04 16:16:27 +0300

    Rework after first review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,8 +1,10 @@
 import os
 
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = False
 
